3e73656f751c8608dc67314b580474f829996f65
julian1 <julian.atkinson71@gmail.com>
2015-09-06 00:27:00 +0000

    update

--- getblock.js
+++ getblock.js
@@ -4,15 +4,9 @@ var web3 = require('web3');
 web3.setProvider(new web3.providers.HttpProvider('http://localhost:8545'));
 
 
-web3.eth.getBlock("latest", function(error, result) {
- 
-  if (!error) {
-    console.log(result);
-  } else {
-    console.log("error " + error);
-  }
-  process.exit();
-}); 
+var latest = web3.eth.getBlock("latest"); 
 
+console.log(latest);
 
+process.exit();
 
